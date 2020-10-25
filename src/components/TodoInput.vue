86e5d25bbd9e3d3da2086dcf957776fbeb7154d1
DESKTOP-0TCN8ST\jhlsm <jhlsmart92@gmail.com>
2020-10-25 23:26:12 +0900

    components implementation

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,9 +1,59 @@
 <template>
-  <div>input</div>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" placeholder="Type what you have to do" v-on:keypress.enter="addTodo">
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="addBtn fa fa-plus" aria-hidden="true"></i>
+    </span>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      newTodoItem: '',
+    }
+  },
+  methods: {
+    addTodo() {
+      //console.log(this.newTodoItem); //this= the component App
+      if (this.newTodoItem !== ""){
+        var value = this.newTodoItem && this.newTodoItem.trim(); // remove the back and forth text in the input box
+        localStorage.setItem(value, value); //setItem(key, Value)
+        this.clearInput(); //initailize the input box input
+      }
+    },
+    clearInput() {
+      this.newTodoItem = '';
+    }
+  }
+};
 </script>
 
-<style></style>
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: inline-block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,9 +1,73 @@
 <template>
-  <div>list</div>
+  <section>
+    <ul>
+      <li v-for="(todoItem, index) in todoItems" class= "shadow"> <!--show local history data on the vue-->
+        <i class = "checkBtn fa fa-check" aria-hidden="true"> </i>
+        {{todoItem}}
+        <span class = "removeBtn" type="button" @click = "removeTodo(todoItem, index)">
+          <i class ="fa fa-trash-o" aria-hidden = "true"></i>
+        </span>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return{
+      todoItems: []// storage for localstorage data
+    }
+  },
+  // push all datas of the localhistory in to todoItems
+  created() {  // after Vue instance created, move data from localhistory to vue data
+    if(localStorage.length > 0) {
+      for(var i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
+  },
+
+  methods: {
+    removeTodo(todoItem,index) { // index is internally managed in the Vue system
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1); //splice is the internally given JS API removing the array factor according to the number
+    }
+  }
+};
 </script>
 
-<style></style>
+<style>
+  ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+  }
+  li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+  }
+  .checkBtn {
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+  }
+  .removeBtn {
+    margin-left: auto;
+    color: #de4343;
+  }
+  .list-enter-active, .list-leave-active {
+    transition: all 1s;
+  }
+  .list-enter, .list-leave-to {
+    opacity: 0;
+    transform: translateY(30px);
+  }
+</style>
